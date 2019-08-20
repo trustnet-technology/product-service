8f0203b32e61df09ae399bc8cc55040a501856f9
zebasc <1929@solvezy.com>
2019-08-20 23:35:06 +0530

    url formating changes

--- src/main/java/com/trustnet/controller/ProductController.java
+++ src/main/java/com/trustnet/controller/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +25,7 @@ import com.trustnet.entity.UserOrder;
 import com.trustnet.services.ProductService;
 
 @RestController
+@RequestMapping("/api/nearby")
 public class ProductController {
 
 	Logger _LOGGER = LoggerFactory.getLogger(ProductController.class);
